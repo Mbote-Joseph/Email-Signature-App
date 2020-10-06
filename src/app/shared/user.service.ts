4f6c6ed6f59d9e781832cbaf9ca6975d931689a6
Mbote-Joseph <mbotejoseph001@gmail.com>
2020-10-06 21:43:30 +0300

    Change of the implementation of the API in enviroment.ts file to store registered users

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 import { Users } from '../Users.model';
 import {map} from 'rxjs/operators';
 import { Observable } from 'rxjs';
@@ -9,15 +9,18 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
 
+  private readonly REST_API_SERVER = "http://localhost:8080/v1";
+  private readonly HEROKU_API_SERVER = "http://emailsignaturegen.herokuapp.com/v1";
 
 
   constructor( private http: HttpClient) { }
 
+  
 
 
   createUser(name: string, company: string, position: string, department: string, phone:string, mobile: string, website: string, skype: string, email: string, address: string) {
     const postUser : Users = {name:name, company:company, position:position, department:department, phone:phone, mobile:mobile, website:website, skype:skype, email:email, address:address};
-    this.http.post<{data:string }>('https://emailsignature-1aa20.firebaseio.com/posts.json', postUser,  {
+    this.http.post<{data:string }>(`${this.HEROKU_API_SERVER}/users`, postUser,  {
       observe: 'response'
     })
     .subscribe(responseData =>{
@@ -26,7 +29,7 @@ export class UserService {
   }
   
   fetchUser(): Observable<any>{
-    return this.http.get<{ [key: string]: Users; }>('https://emailsignature-1aa20.firebaseio.com/posts.json')
+    return this.http.get<{ [key: string]: Users; }>(`${this.HEROKU_API_SERVER}/users`)
       .pipe(
         map(responseData => {
           const userArray: Users[] = [];
@@ -41,6 +44,8 @@ export class UserService {
      //.subscribe(posts=>{});
   }
   deleteUser(){
-    return this.http.delete('https://emailsignature-1aa20.firebaseio.com/posts.json');
+    return this.http.delete(`${this.HEROKU_API_SERVER}/users`);
   }
 }
+
+//`${this.HEROKU_API_SERVER}/users`
